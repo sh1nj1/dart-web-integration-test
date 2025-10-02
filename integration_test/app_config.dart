ce5fa8ad323ad909cf5fa6d8728f757c7c9d28b0
Soonoh <soonoh.jung@gmail.com>
2025-10-02 18:18:10 +0900

    remove generated files

--- integration_test/app_config.dart
+++
@@ -1,9 +0,0 @@
-// Auto-generated app configuration
-import 'package:flutter_test/flutter_test.dart';
-import 'package:test_target/main.dart' as app;
-
-/// Initialize and start the app for testing
-Future<void> startApp(WidgetTester tester) async {
-  app.main();
-  await tester.pumpAndSettle();
-}
